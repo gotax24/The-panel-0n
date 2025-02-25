52eac65362869cb3eb854b90df582bad364a8a06
gotax24 <124403304+gotax24@users.noreply.github.com>
2025-02-25 11:23:43 -0400

    agregando auth0 al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -9,21 +9,29 @@ import Task from "./components/Task.jsx";
 import ThemeProvider from "./context/ThemeProvider.jsx";
 import "./index.css";
 import Users from "./components/Users.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const callbackUrl = "http://localhost:5173/"
+
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="tasks" element={<Task />} />
-          <Route path="statistics" element={"Estadisticas"} />
-          <Route path="users" element={<Users />} />
-        </Route>
-        <Route path="/CreateAnAccount" element={<CreateAnAccount />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>
+  <Auth0Provider domain={domain} clientId={clientId} callbackUrl={callbackUrl}>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="tasks" element={<Task />} />
+            <Route path="statistics" element={"Estadisticas"} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="/CreateAnAccount" element={<CreateAnAccount />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </Auth0Provider>
 );
